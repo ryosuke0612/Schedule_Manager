24f7d4cda9399742192ef06579f1e0349296004e
ryosuke0612 <ponntann2003@yahoo.co.jp>
2025-05-03 21:47:16 +0900

    Update app.py~

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 from models import db, Event, User, Attendance
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+# データベースの絶対パスを指定
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 
 with app.app_context():
